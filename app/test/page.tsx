af516537e5fcbcf79f9f9b2d6223c160429496e3
若林将輝 <masaki.wakabaya@gmail.com>
2023-08-15 13:25:35 +0900

    sanctum spa auth

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Test from '@/components/Test';
+
+const Page = () => {
+
+    return (
+        <div>
+            <Test />
+        </div>
+    );
+}
+
+export default Page;
---
+++ components/Test.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+    // XSRF-TOKENをリクエスト時に送信するための設定
+    const http = axios.create({
+        baseURL: 'http://localhost:8080',
+        withCredentials: true,
+    });
+
+const Test = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const postData = async () => {
+        axios.get('http://localhost:8080/sanctum/csrf-cookie').then((res: any) => {
+            console.log(res);
+            http.post('/api/login', {email, password}).then((res: any) => {
+                console.log(res);
+            })
+        });
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 m-3 max-w-sm'
+                placeholder='email'
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                }}
+            />
+            <input
+                type="text"
+                className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 m-3 max-w-sm'
+                placeholder='password'
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+            />
+            <div>
+                <button
+                    className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded m-3"
+                    onClick={()=>{
+                        postData();
+                    }}
+                >送信</button>
+            </div>
+        </div>
+    );
+}
+
+export default Test;
